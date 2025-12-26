90186539a268a5a4a2985ff6e607814f47b52042
Glauber Torres <gthierrytorres@gmail.com>
2025-12-25 22:28:27 -0300

    docs: reverte alteração no requirements

--- backend/app/database/client.py
+++ backend/app/database/client.py
@@ -14,7 +14,8 @@ def connect_to_mongo():
         db = client[settings.DB_NAME]
         print(f"MongoDB conectado com sucesso ao banco: {settings.DB_NAME}")
     except Exception as e:
-        print(f"ERRO DE CONEXÃO COM MONGODB: {e}")
+        print(f"ERRO DE CONEXÃO COM MONGODB: {str(e)}")
+        raise e
 
 def close_mongo_connection():
     """Fecha a conexão com o MongoDB."""
