bedf02a1b75928a8c4b964a1f6914e21d309c2ab
Glauber Torres <gthierrytorres@gmail.com>
2026-01-01 13:34:57 -0300

    chore: ajuste de estilo nos links

--- frontend/src/pages/Estoque.js
+++ frontend/src/pages/Estoque.js
@@ -347,7 +347,7 @@ const Estoque = () => {
                           {produto.codigo_lm}
                         </a>
                       ) : (
-                        <span style={{ color: 'black' }}>
+                        <span>
                           {produto.codigo_lm}
                         </span>
                       )}
@@ -364,7 +364,7 @@ const Estoque = () => {
                           {produto.nome_produto}
                         </a>
                       ) : (
-                        <span style={{ color: 'black' }}>
+                        <span>
                           {produto.nome_produto}
                         </span>
                       )}
